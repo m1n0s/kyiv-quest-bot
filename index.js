22f33d8c5e75f031b12c5b3ec01148d73ee245bc
m1n0s <leontiy1992@gmail.com>
2017-03-23 23:40:58 +0200

    get location from user

--- index.js
+++ index.js
@@ -3,25 +3,30 @@ import token from './token';
 
 const bot = new TelegramBot(token, { polling: true });
 
-
-// Matches "/echo [whatever]"
-bot.onText(/\/echo (.+)/, (msg, match) => {
-  // 'msg' is the received Message from Telegram
-  // 'match' is the result of executing the regexp above on the text content
-  // of the message
-
-  const chatId = msg.chat.id;
-  const resp = match[1]; // the captured "whatever"
-
-  // send back the matched "whatever" to the chat
-  bot.sendMessage(chatId, resp);
-});
-
-// Listen for any kind of message. There are different kinds of
-// messages.
 bot.on('message', msg => {
   const chatId = msg.chat.id;
 
-  // send a message to the chat acknowledging receipt of their message
-  bot.sendMessage(chatId, 'Received your message');
+  const location = msg.location;
+
+  if (location) {
+    console.log(location);
+  }
+
+  //bot.sendMessage(chatId, 'Received your message');
+});
+
+bot.onText(/\/loc/, msg => {
+  const { id } = msg.chat;
+
+  const opts = {
+    reply_markup: JSON.stringify({
+      keyboard: [
+        [{ text: 'Send Location', request_location: true }],
+        [{ text: 'Send Contact', request_contact: true }],
+      ],
+      one_time_keyboard: true,
+    }),
+  };
+
+  bot.sendMessage(id, 'Need you location', opts);
 });
