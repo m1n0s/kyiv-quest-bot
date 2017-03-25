c3fdab04477d1056710e6f26d4097d4c68277f8b
m1n0s <leontiy1992@gmail.com>
2017-03-25 21:18:13 +0200

    WIP migrate to async/await

--- index.js
+++ index.js
@@ -8,12 +8,14 @@ const map = new GoogleMapsAPI({
   key: GOOGLE_MAPS_API_KEY,
 });
 
+let game = {};
+
 bot.on('message', msg => {
   const chatId = msg.chat.id;
 
   const location = msg.location;
 
-  if (location) {
+  /*if (location) {
     const { latitude, longitude } = location;
 
     const reverseGeocodeParams = {
@@ -37,7 +39,7 @@ bot.on('message', msg => {
       console.log(bestResult.address_components);
       bot.sendMessage(chatId, bestResult.formatted_address);
     });
-  }
+  }*/
 });
 
 bot.onText(/\/loc/, msg => {
@@ -55,3 +57,92 @@ bot.onText(/\/loc/, msg => {
 
   bot.sendMessage(id, 'Need you location', opts);
 });
+
+bot.onText(/\/start/, async msg => {
+  const { first_name: firstName, last_name: lastName, id } = msg.from;
+
+  const opts = {
+    reply_markup: JSON.stringify({
+      keyboard: [
+        [{ text: 'Так', request_location: true }],
+      ],
+      one_time_keyboard: true,
+    }),
+  };
+
+  const sendedMessage = await bot.sendMessage(
+    id,
+    `Привіт ${firstName} ${lastName}! Для того щоб почати квест ти маєш бути на вулиці Михайла Котельнікова, 5. Ти вже на місці?`,
+    opts,
+  );
+
+  console.log('sendedMessage');
+  console.log(sendedMessage);
+
+  /*bot.sendMessage(
+    id,
+    `Привіт ${firstName} ${lastName}! Для того щоб почати квест ти маєш бути на вулиці Михайла Котельнікова, 5. Ти вже на місці?`,
+    opts,
+  )
+    .then(message => {
+      console.log(message);
+      const msgId = message.message_id;
+
+      bot.onReplyToMessage(id, msgId, reply => {
+
+        const { latitude, longitude } = reply.location;
+
+        const reverseGeocodeParams = {
+          latlng: `${latitude},${longitude}`,
+          language: 'uk',
+        };
+
+        map.reverseGeocode(reverseGeocodeParams, (err, result) => {
+
+          if (err) {
+            console.log(err);
+            return;
+          }
+
+          if (result.status !== 'OK') {
+            return;
+          }
+
+          const bestResult = result.results[0];
+
+          console.log(bestResult.address_components);
+          bot.sendMessage(id, bestResult.formatted_address);
+        });
+
+        //console.log(reply);
+      });
+    });*/
+
+
+  //console.log(msg.from);
+  //console.log(msg.chat);
+});
+
+/*
+async function onStartQuest(msg) {
+  const { first_name: firstName, last_name: lastName, id } = msg.from;
+
+  const opts = {
+    reply_markup: JSON.stringify({
+      keyboard: [
+        [{ text: 'Так', request_location: true }],
+      ],
+      one_time_keyboard: true,
+    }),
+  };
+
+  const sendedMessage = await bot.sendMessage(
+    id,
+    `Привіт ${firstName} ${lastName}! Для того щоб почати квест ти маєш бути на вулиці Михайла Котельнікова, 5. Ти вже на місці?`,
+    opts,
+  );
+
+  console.log(sendedMessage);
+}
+
+bot.onText(/\/start/, onStartQuest);*/
