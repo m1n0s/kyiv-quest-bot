362987156a95ad18123bc121e118769668cf9c9e
m1n0s <leontiy1992@gmail.com>
2017-03-27 23:16:11 +0300

    use async/await for promises

--- index.js
+++ index.js
@@ -8,15 +8,47 @@ const map = new GoogleMapsAPI({
   key: GOOGLE_MAPS_API_KEY,
 });
 
-let game = {};
+// bot.on('message', msg => {});
 
-bot.on('message', msg => {
-  const chatId = msg.chat.id;
+bot.onText(/\/loc/, msg => {
+  const { id } = msg.chat;
 
-  const location = msg.location;
+  const opts = {
+    reply_markup: JSON.stringify({
+      keyboard: [
+        [{ text: 'Send Location', request_location: true }],
+        [{ text: 'Send Contact', request_contact: true }],
+      ],
+      one_time_keyboard: true,
+    }),
+  };
 
-  /*if (location) {
-    const { latitude, longitude } = location;
+  bot.sendMessage(id, 'Need you location', opts);
+});
+
+bot.onText(/\/start/, async msg => {
+  const { first_name: firstName, last_name: lastName, id: chatId } = msg.from;
+
+  const opts = {
+    reply_markup: JSON.stringify({
+      keyboard: [
+        [{ text: 'Так', request_location: true }],
+      ],
+      one_time_keyboard: true,
+    }),
+  };
+
+  const sendedMessage = await bot.sendMessage(
+    chatId,
+    `Привіт ${firstName} ${lastName}! Для того щоб почати квест ти маєш бути на вулиці Михайла Котельнікова, 5. Ти вже на місці?`,
+    opts,
+  );
+
+  const { message_id: msgId } = sendedMessage;
+
+  bot.onReplyToMessage(chatId, msgId, reply => {
+
+    const { latitude, longitude } = reply.location;
 
     const reverseGeocodeParams = {
       latlng: `${latitude},${longitude}`,
@@ -39,110 +71,5 @@ bot.on('message', msg => {
       console.log(bestResult.address_components);
       bot.sendMessage(chatId, bestResult.formatted_address);
     });
-  }*/
+  });
 });
-
-bot.onText(/\/loc/, msg => {
-  const { id } = msg.chat;
-
-  const opts = {
-    reply_markup: JSON.stringify({
-      keyboard: [
-        [{ text: 'Send Location', request_location: true }],
-        [{ text: 'Send Contact', request_contact: true }],
-      ],
-      one_time_keyboard: true,
-    }),
-  };
-
-  bot.sendMessage(id, 'Need you location', opts);
-});
-
-bot.onText(/\/start/, async msg => {
-  const { first_name: firstName, last_name: lastName, id } = msg.from;
-
-  const opts = {
-    reply_markup: JSON.stringify({
-      keyboard: [
-        [{ text: 'Так', request_location: true }],
-      ],
-      one_time_keyboard: true,
-    }),
-  };
-
-  const sendedMessage = await bot.sendMessage(
-    id,
-    `Привіт ${firstName} ${lastName}! Для того щоб почати квест ти маєш бути на вулиці Михайла Котельнікова, 5. Ти вже на місці?`,
-    opts,
-  );
-
-  console.log('sendedMessage');
-  console.log(sendedMessage);
-
-  /*bot.sendMessage(
-    id,
-    `Привіт ${firstName} ${lastName}! Для того щоб почати квест ти маєш бути на вулиці Михайла Котельнікова, 5. Ти вже на місці?`,
-    opts,
-  )
-    .then(message => {
-      console.log(message);
-      const msgId = message.message_id;
-
-      bot.onReplyToMessage(id, msgId, reply => {
-
-        const { latitude, longitude } = reply.location;
-
-        const reverseGeocodeParams = {
-          latlng: `${latitude},${longitude}`,
-          language: 'uk',
-        };
-
-        map.reverseGeocode(reverseGeocodeParams, (err, result) => {
-
-          if (err) {
-            console.log(err);
-            return;
-          }
-
-          if (result.status !== 'OK') {
-            return;
-          }
-
-          const bestResult = result.results[0];
-
-          console.log(bestResult.address_components);
-          bot.sendMessage(id, bestResult.formatted_address);
-        });
-
-        //console.log(reply);
-      });
-    });*/
-
-
-  //console.log(msg.from);
-  //console.log(msg.chat);
-});
-
-/*
-async function onStartQuest(msg) {
-  const { first_name: firstName, last_name: lastName, id } = msg.from;
-
-  const opts = {
-    reply_markup: JSON.stringify({
-      keyboard: [
-        [{ text: 'Так', request_location: true }],
-      ],
-      one_time_keyboard: true,
-    }),
-  };
-
-  const sendedMessage = await bot.sendMessage(
-    id,
-    `Привіт ${firstName} ${lastName}! Для того щоб почати квест ти маєш бути на вулиці Михайла Котельнікова, 5. Ти вже на місці?`,
-    opts,
-  );
-
-  console.log(sendedMessage);
-}
-
-bot.onText(/\/start/, onStartQuest);*/
